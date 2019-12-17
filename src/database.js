088792b670ea97869bed4337b4fa96ce7a565bba
mujd <mujd19@gmail.com>
2019-12-17 17:12:14 -0300

    Se agrega docker, se agrega mejoras en manejo conexion a mongodb

--- src/database.js
+++ src/database.js
@@ -1,13 +1,40 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const mongoDB = async(res, err) => {
-    await mongoose.connect('mongodb://localhost:27017/segurosFalabellaDB', {
-        // useCreateIndex: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-    if (err) throw err;
-    console.log("Database: \x1b[32m%s\x1b[0m", "online");
+mongoose.Promise = Promise
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongo Connection Established')
+})
+
+mongoose.connection.on('reconnected', () => {
+    console.log('Mongo Connection Reestablished')
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongo Connection Disconnected')
+})
+
+mongoose.connection.on('close', () => {
+    console.log('Mongo Connection Closed')
+})
+
+mongoose.connection.on('error', (error) => {
+    console.log('Mongo ERROR: ' + error)
+})
+
+let mongoUrl = 'localhost';
+if (process.env.DATABASE_HOST) {
+    mongoUrl = 'mongo';
 }
+const mongoDB = async() => {
+    await mongoose.connect(`mongodb://${mongoUrl}:27017/segurosFalabellaDB`, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    }).catch(err => console.log('Mongo error: ' + err));
+}
+
+mongoDB().catch(error => console.log('Mongo error: ' + error))
 
 module.exports = { mongoDB };
